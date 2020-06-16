fc79b6b15eefccdd2abfe32916a414bff24b2327
Chloe Steffens <caforonda103@yahoo.com>
2020-06-16 18:11:53 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
   
   def self.most_popular_show
-    self.where("rating" = ?, self.maximum("rating"))
+    self.where("rating = ?", self.maximum("rating"))
   end
   
   def self.lowest_rating
@@ -13,7 +13,7 @@ class Show < ActiveRecord::Base
   end
   
   def self.least_popular_show
-    self.minimum("popularity")
+    self.where("rating = ?", self.maximum("rating"))
   end
   
   def self.ratings_sum
