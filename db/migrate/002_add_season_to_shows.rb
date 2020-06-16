f829ca90cf83e87fcf159afcf4b5629c56646d89
Chloe Steffens <caforonda103@yahoo.com>
2020-06-16 16:51:42 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,11 +1,7 @@
 class AddSeasonToShows < ActiveRecord::Migration[5.2]
   
   def add_column
-    create_table :shows do |t|
-      t.string :name
-      t.string :network
-      t.string :day
-      t.integer :rating
+    
     end
   end
   
