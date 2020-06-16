fbf4a4e7f1c6c0ce1be432d025e12acc28f67cfc
Chloe Steffens <caforonda103@yahoo.com>
2020-06-16 18:10:48 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,4 +4,20 @@ class Show < ActiveRecord::Base
     self.maximum("rating")
   end
   
+  def self.most_popular_show
+    self.maximum("popularity")
+  end
+  
+  def self.lowest_rating
+    self.minimum("rating")
+  end
+  
+  def self.least_popular_show
+    self.minimum("popularity")
+  end
+  
+  def self.ratings_sum
+    
+  end
+  
 end
